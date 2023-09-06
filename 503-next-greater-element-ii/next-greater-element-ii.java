250372abd4da1b004957327005429e06d14058a0
Akshay RC <er.akshayrc@gmail.com>
2023-09-06 16:35:53 -0700

    Time: 10 ms (86.69%) | Memory: 44.6 MB (83.64%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -6,6 +6,10 @@ class Solution {
         Arrays.fill(results, -1);
         Stack<Integer> st = new Stack<>();
         for(int i=0; i<2*n;i++){
+            if(i>n){
+                if(st.peek() == i%n)
+                break;
+            }
             while(!st.isEmpty() && nums[st.peek()] < nums[i%n]){
                 int popped = st.pop();
                 results[popped] = nums[i%n];
